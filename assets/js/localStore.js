ef20caa28c23905a1704932237ef6a451a76f3d8
bbrazis <32001385+bbrazis@users.noreply.github.com>
2023-10-12 15:08:42 -0700

    Update localStore.js

--- assets/js/localStore.js
+++ assets/js/localStore.js
@@ -1,12 +1,12 @@
+const getWatchList = () => {
+    return JSON.parse( localStorage.getItem('watchlist') )
+}
+
 const addToStorage = item => {
     let array = []
     array = getWatchList()
     const itemToString = JSON.stringify(array.push(item.imdbID))
     localStorage.setItem('watchlist', itemToString)
-}
-
-const getWatchList = () => {
-    return JSON.parse( localStorage.getItem('watchlist') )
 }
 
 export { addToStorage, getWatchList }
