4d9d2b50a6ac803e30e595e11fc654c13a1f703e
bbrazis <32001385+bbrazis@users.noreply.github.com>
2023-10-16 14:43:03 -0700

    Update localStore.js

--- assets/js/localStore.js
+++ assets/js/localStore.js
@@ -2,8 +2,7 @@ const addToStorage = (array,item) => {
     const filter = array.filter(movie => movie.Title === item.Title)[0]
     if(!array.includes(filter)){
         array.push(item)
-        const itemToAdd = array
-        localStorage.setItem('watchlist', JSON.stringify(itemToAdd))
+        localStorage.setItem('watchlist', JSON.stringify(array))
     } else {
         alert(`Sorry, ${item.Title} is already on your Watchlist`)
     }
@@ -13,6 +12,7 @@ const removeFromStorage = (array, id) => {
     const itemToRemove = array.filter(movie => movie.imdbID === id)[0]
     const index = array.indexOf(itemToRemove)
     array.splice(index,1)
+    localStorage.setItem('watchlist', JSON.stringify(array))
 }
 
 export { addToStorage, removeFromStorage }
