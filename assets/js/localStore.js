074ac1f12868f8596d0d5129d4c2ea4dacd03937
bbrazis <32001385+bbrazis@users.noreply.github.com>
2023-10-16 14:18:08 -0700

    Update localStore.js

--- assets/js/localStore.js
+++ assets/js/localStore.js
@@ -10,14 +10,15 @@ const addToStorage = (array,item) => {
 }
 
 const removeFromStorage = (array) => {
-    const id = this.id
+    const id = this
+    console.log(id)
     // const itemToRemove = watchlist.filter(movie => movie.imdbID === id)[0]
-    for (let [index, movie] of array.entries) {
-        if(movie.imdbID === id){
-            array.splice(index, 1)
-            localStorage.setItem('watchlist', JSON.stringify(array))
-        }
-    }
+    // for (let [index, movie] of array.entries) {
+    //     if(movie.imdbID === id){
+    //         array.splice(index, 1)
+    //         localStorage.setItem('watchlist', JSON.stringify(array))
+    //     }
+    // }
 }
 
 export { addToStorage, removeFromStorage }
