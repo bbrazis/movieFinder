7372624620a3fbd4c9d3c6df008ed7cd40e6e9ef
bbrazis <32001385+bbrazis@users.noreply.github.com>
2023-10-16 11:18:26 -0700

    Update localStore.js

--- assets/js/localStore.js
+++ assets/js/localStore.js
@@ -1,8 +1,8 @@
 const addToStorage = (array,item) => {
     // const id = item.imdbID
     const index = array.length + 1
-
-    const itemToAdd = array.push(JSON.stringify(item))
+    const obj = {item}
+    const itemToAdd = array.push(JSON.stringify(obj))
     console.log(item)
     console.log(itemToAdd)
     const itemToString = JSON.parse(itemToAdd)
