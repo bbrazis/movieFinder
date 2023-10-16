f3875ab249e77d92b265fd80cdb62b11011a9da1
bbrazis <32001385+bbrazis@users.noreply.github.com>
2023-10-16 12:14:00 -0700

    Update watchlist.js

--- assets/js/watchlist.js
+++ assets/js/watchlist.js
@@ -3,3 +3,15 @@ import { createWatchlist } from '/assets/js/builders.js'
 import { key, url } from '/assets/js/yek.js'
 
 const movieList = document.getElementById('movie-list')
+
+const initializeList = async () => {
+    movieList.innerHTML = ''
+    const placeholder = document.querySelector('.main-load')
+    placeholder.classList.add('display-none')
+    
+    for(let item of watchlist){
+        populateByIndex(item.imdbID)
+    }
+}
+
+initializeList()
