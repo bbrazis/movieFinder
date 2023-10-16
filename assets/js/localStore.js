e6c6708f461286f7738da6a201fa5e2eac3f35f2
bbrazis <32001385+bbrazis@users.noreply.github.com>
2023-10-16 11:35:59 -0700

    Update localStore.js

--- assets/js/localStore.js
+++ assets/js/localStore.js
@@ -1,11 +1,10 @@
 const addToStorage = (array,item) => {
-    // const id = item.imdbID
     const itemToAdd = array.concat([item])
     console.log(item)
     console.log(itemToAdd)
     const itemToString = JSON.stringify(itemToAdd)
     console.log(itemToString)
-    // localStorage.setItem('watchlist', itemToString)
+    localStorage.setItem('watchlist', itemToString)
 }
 
 export { addToStorage }
