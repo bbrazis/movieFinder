932d44521a44ce605fdc286b4815be6d46fa0385
bbrazis <32001385+bbrazis@users.noreply.github.com>
2023-10-16 12:00:22 -0700

    Update localStore.js

--- assets/js/localStore.js
+++ assets/js/localStore.js
@@ -1,6 +1,6 @@
 const addToStorage = (array,item) => {
-    const filter = array.filter(movie => movie === item)
-    if(filter){
+    const filter = array.filter(movie => movie === item)[0]
+    if(!filter){
         array.push(item)
         const itemToAdd = array
         localStorage.setItem('watchlist', JSON.stringify(itemToAdd))
