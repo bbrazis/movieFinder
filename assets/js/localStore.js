083495b31bf118a76118a0e81f5e047b807fe70f
bbrazis <32001385+bbrazis@users.noreply.github.com>
2023-10-16 14:35:39 -0700

    Update localStore.js

--- assets/js/localStore.js
+++ assets/js/localStore.js
@@ -13,6 +13,8 @@ const removeFromStorage = (array, id) => {
     console.log(id)
     const itemToRemove = array.filter(movie => movie.imdbID === id)[0]
     console.log(itemToRemove)
+    const index = array.indexOf(itemToRemove)
+    console.log(index)
     // for (let [index, movie] of array) {
     //     if(movie.imdbID === id){
     //         array.splice(index, 1)
